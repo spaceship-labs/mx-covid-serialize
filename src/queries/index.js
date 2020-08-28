ef1e767fd2d442a2100be9c64ecdf0f0b408dd02
Pablo Melendez <pablofer.melendez@gmail.com>
2020-08-28 12:48:45 -0500

    add unimported query set

--- src/queries/index.js
+++ src/queries/index.js
@@ -1,5 +1,8 @@
 import days from './days';
 import govData from './govData';
 import municipalities from './municipalities';
+import mexData from './mexData';
 
-export default { days, govData, municipalities };
+export default {
+  days, govData, municipalities, mexData,
+};
